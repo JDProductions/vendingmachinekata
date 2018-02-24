83cb77c1f6b6acd69bcfe9a773a0c2390b35b12f
JamesDuBois1994 <duboisprogramming@gmail.com>
2018-02-24 17:12:20 -0500

    Green State - whenIInsertMoneyAndPushReturnCoinButtonIShouldGetAllOfMyMoneyBack
    This verifies if you deposit money and hit the return coin button, you will be returned your money in the coin return.

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -69,6 +69,9 @@ public class VendingMachine {
             wasCandyButtonPressed = true;
             dispense(candyItemID);
         }
+        else {
+            coinReturnAmount += this.totalAmountDeposited;
+        }
     }
 
 
